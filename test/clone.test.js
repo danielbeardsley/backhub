bdd6fe23abf724d1f2c43a96ffd5ac7df316f927
Daniel Beardsley <daniel@ifixit.com>
2013-11-15 00:28:54 -0800

    Make git clone work (+ tests)
    Add a git repo for testing so we can clone something that exists.

--- test/clone.test.js
+++ test/clone.test.js
@@ -1,7 +1,8 @@
 var Backhub      = require('../lib/backhub'),
     fs           = require('fs'),
-    assert       = require('assert')
-    // childProcess = require('child_process'),
+    assert       = require('assert'),
+    Q            = require('q'),
+    exec         = require('child_process').exec,
     testRepoSource   = __dirname + "/../fixtures/repo/"
 
 // Current branch pointers in our test repo.
@@ -9,16 +10,21 @@ var testBranch = "aa6b0aa64229caee1b07500334a64de9e1ffcddd",
     master     = "ff47c0e58eef626f912c7e5d80d67d8796f65003"
 
 describe("backhub", function() {
+   before(setupFixture);
+
    var backupDest = tempDir()
-   fs.mkdir(backupDest);
+   fs.mkdirSync(backupDest);
    var bh = new Backhub({
       destination: backupDest
    })
 
    it("should clone any repos it doesn't know about", function(done) {
+      this.timeout(5000);
       bh.inject(postReceive("repo", "user"), function() {
          assert.fs.exists(backupDest + "/user/repo")
-         done()
+         assert.fs.isGitRepo(backupDest + "/user/repo").then(function() {
+            done()
+         }).done();
       })
    })
 })
@@ -34,6 +40,21 @@ describe("Misconfigured backhub", function() {
    })
 })
 
+
+/**
+ * Because Git doesn't allow adding any files or dirs named .git
+ * we can't add the test repo's .git dir directly to the main repo.
+ * We must resort to dynamically creating and destroying a
+ * "symlink-like" file that points git to a different folder for
+ * the actual repo dir.
+ *
+ * Actual repo dir:        fixtures/repo/git
+ * "symlink" to repo dir:  fixtures/repo/.git
+ */
+function setupFixture() {
+   // Add a 'git-style' symlink
+   fs.writeFileSync(testRepoSource + '.git', "gitdir: ./git");
+};
 
 /**
  * A Stripped-down version of the the JSON from a post-receive event.
@@ -68,4 +89,16 @@ assert.fs = {
    exists: function(path) {
       assert.ok(fs.existsSync(path), "Should exist: " + path)
    }
+}
+
+assert.fs.isGitRepo = function(dir) {
+   var deferred = Q.defer(); 
+   exec("git remote -v", {cwd: dir}, function (err, output) {
+      if (err) {
+         assert(false, dir + " is not a git repo")
+      } else {
+         deferred.resolve(output);
+      }
+   });
+   return deferred.promise;
 }
