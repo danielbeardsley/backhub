f67321df413d6a2b9df72a69a1ed55eb1474078c
Daniel Beardsley <daniel@ifixit.com>
2014-01-11 00:51:27 -0700

    cli tests: update for required 'destination' param

--- test/cli.test.js
+++ test/cli.test.js
@@ -8,22 +8,16 @@ var BackhubModule = require('../lib/backhub'),
 
 describe("backhub cli", function(done) {
    it("should use the default port when none is specified", function(done) {
-      simulateExec(['./backhub']).then(function(config) {
-         assert.deepEqual({port:defaultPort, destination:undefined}, config);
+      simulateExec(['./backhub', '--destination', 'dir']).then(function(config) {
+         assert.deepEqual({port:defaultPort, destination:'dir'}, config);
          done();
       }).done();
    });
 
    it("should use the port specified by the commandline option", function(done) {
-      simulateExec(['./backhub', '--port', testPort]).then(function(config) {
-         assert.deepEqual({port:testPort, destination:undefined}, config);
-         done();
-      }).done();
-   });
-
-   it("should use the destination specified by the 'destination' argument", function(done) {
-      simulateExec(['./backhub', '--destination', 'dir']).then(function(config) {
-         assert.deepEqual({port:defaultPort, destination:'dir'}, config);
+      simulateExec(['./backhub', '--port', testPort, '--destination', 'dir']).
+      then(function(config) {
+         assert.deepEqual({port:testPort, destination:'dir'}, config);
          done();
       }).done();
    });
